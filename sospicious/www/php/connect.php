aea8e2d4071aaab74956af4c1853b9f08ede289d
rhayesantillan <rkssantillan@iskolarngbayan.pup.edu.ph>
2023-07-06 13:46:08 +0800

    push ko daw

---
+++ sospicious/www/php/connect.php
@@ -0,0 +1,17 @@
+<?php
+    require_once "../php/config.php";
+
+    $serverName = DB_HOST;
+    $username = DB_USER;
+    $password = DB_PASS;
+    $dbName = DB_NAME; 
+
+    try{
+        $options = array(PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        $conn = new PDO("mysql:host=$serverName;dbname=$dbName",$username, $password, $options);
+        echo "Connection Success";
+    }
+    catch(PDOException $e){
+        echo "Error in connection" . $e->getMessage();
+    }
+?>
