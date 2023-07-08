30365a05b5723bc83234b6bb1bda5f847b87f2fd
Jessie Bandol <jessiebandol2900@gmail.com>
2023-07-08 13:39:55 +0800

    commute2
    commute2

--- sospicious/www/templates/commute2.php
+++ sospicious/www/templates/commute2.php
@@ -1,3 +1,45 @@
+<?php
+    // session_start();
+    // include("../php/connect.php");
+
+    // if($_SERVER['REQUEST_METHOD'] == "POST"){
+    //     //something was posted
+
+    //     $ride_id = $_POST["commute2_input_loc"];
+    //     $user_id = $_POST["password"];
+    //     $firstname = $_POST["firstname"];
+    //     $lastname = $_POST["lastname"];
+    //     $email = $_POST["email"];
+    //     $mobile = $_POST["mobile"];
+    //     $birthdate = $_POST["birthdate"];
+
+    //     if (!empty($username) && !empty($password) 
+    //     && !empty($firstname) && !empty($lastname) 
+    //     && !empty($email) && !empty($mobile) 
+    //     && !empty($birthdate)) {
+    //         if (!filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+    //             echo "Invalid email format!";
+    //             die;
+    //         }else{
+
+    //         }
+
+    //         //save to database
+    //         $query = "INSERT INTO sos_rides (username, password, user_fname, user_lname, user_email, user_mobile, user_birthdate) 
+    //             VALUES ('$username', '$password', '$firstname', '$lastname', '$email', '$mobile', '$birthdate')";
+
+    //         mysqli_query($conn, $query);
+
+    //         // header("Location: ../templates/index.php");
+    //         // die;
+    //    }else{
+    //     echo "Please fill in all the fields!";
+    //    }
+    // }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,18 +68,30 @@
         </div>
 
         <div class="commute2_loc">
-            <img class="commute2_pin" src="../img/map-pin-c.svg" alt="map-pin">
-            <input class="commute2_input" type="text" required>
-            <br>
-            <img class="commute2_taxi" src="../img/ph_taxi-light.svg" alt="taxi">
-            <input class="commute2_input" type="text" required>
+            <div class="commute2_inline">
+                <div>
+                    <img class="commute2_pin" src="../img/map-pin-c.svg" alt="map-pin">
+                </div>
+                <div>
+                    <input class="commute2_input" id="commute2_input_loc" type="text" size="25" required>
+                </div>
+            </div>
+            <!-- <br> -->
+            <div class="commute2_inline">
+                <div>
+                    <img class="commute2_taxi" src="../img/ph_taxi-light.svg" alt="taxi">
+                </div>
+                <div>
+                    <input class="commute2_input" id="commute2_time" type="text" size="25" required>
+                </div>
+            </div>
         </div>
         <form action="#" class="form">
             <div class="commute2_column">
                 <div class="commute2_col">
                     <label class="commute2_label" for="commute2_vehicle">Type</label>
                     <div class="commute2_dropdown">
-                    <select name="commute2_vehicle" id="commute2_option" class="commute2_same" required>
+                    <select name="commute2_vehicle" id="commute2_option" class="commute2_same" required id="commute2_transpo" >
                         <option value="Taxi">Taxi</option>
                         <option value="Bus">Bus</option>
                         <option value="Tricycle">Tricycle</option>
@@ -71,11 +125,19 @@
         </div>
 
         <div class="commute2_two_buttons">
-            <button class="commute2_share">Share</button>
+            <!-- <button onclick="myFunction" type="submit" class="commute2_share">Share</button> -->
+            <input type="submit" value="share" class="commute2_share" onclick="myFunction()">
             <button class="commute2_cancel">Cancel</button>
+        </div>
+
+        <div class="message">
+                <div class="success" id="success">Your Message Successfully Sent!</div>
+                <div class="danger" id="danger">Fields Can't be Empty!</div>
         </div>
         
 
     </div>
+
+    <script src="../js/commute.js"></script>
 </body>
 </html>
